04b16b71d88ec6eb24639223fc7b8961108c2a59
Caro <55109108+CaroMen@users.noreply.github.com>
2020-11-15 20:35:31 -0600

    Initial Commit

---
+++ Shared/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  Shared
+//
+//  Created by Caro on 11/15/20.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
